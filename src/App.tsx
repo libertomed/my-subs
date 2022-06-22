5e634030a9ef0e9b50e8ad34fc6374ede0d69634
maxmurdox <libertomed@gmail.com>
2022-06-22 20:56:13 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import {useState, useEffect} from 'react';
+import List from './components/List'
 import './App.css';
 
+interface Sub {
+  nick: string
+  avatar: string
+  subMonths: number
+  description?: string
+}
+
+interface AppState {
+  subs: Sub[]
+  newSubsNumber: number
+}
+
+const INITIAL_STATE = [
+  { 
+    nick: 'liber',
+    subMonths: 3,
+    avatar: 'https://i.pravatar.cc/150?u=liberto',
+    description: 'Liberto description del primer avatar',
+  },
+  { 
+    nick: 'juan',
+    subMonths: 5,
+    avatar: 'https://i.pravatar.cc/150?u=juan',
+  },
+]
+
 function App() {
+  const [subs, setSubs] = useState<AppState['subs']>([])
+
+  const [newSubsNumber, setNewSubsNumber] = useState<AppState['newSubsNumber']>(0)
+
+  useEffect(() => {
+    setSubs(INITIAL_STATE)
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Liber subs</h1>
+      <List subs={subs}/>
     </div>
   );
 }
